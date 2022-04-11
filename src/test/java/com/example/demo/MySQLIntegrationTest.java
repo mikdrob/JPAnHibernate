ea3e2b18c6d2f7d6359a5270b0ff242ce03ff573
mikdrob <mgdrobyshev@gmail.com>
2022-04-11 14:43:00 +0300

    configure schema

--- src/test/java/com/example/demo/MySQLIntegrationTest.java
+++ src/test/java/com/example/demo/MySQLIntegrationTest.java
@@ -1,4 +1,29 @@
 package com.example.demo;
 
+import com.example.demo.repository.BookRepository;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@ActiveProfiles("local")
+@DataJpaTest
+@ComponentScan(basePackages = {"com.example.demo.bootstrap"})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class MySQLIntegrationTest {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    void testMySQL(){
+        long countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(2);
+    }
 }
--- src/test/java/com/example/demo/SpringBootJpaTestSlice.java
+++ src/test/java/com/example/demo/SpringBootJpaTestSlice.java
@@ -9,15 +9,18 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.annotation.Commit;
 import org.springframework.test.annotation.Rollback;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DataJpaTest
 @ComponentScan(basePackages = {"com.example.demo.bootstrap"})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class SpringBootJpaTestSlice {
 
     @Autowired
